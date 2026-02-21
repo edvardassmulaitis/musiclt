724dfefb0206f6111a57db160d3508b63ab35e8e
edvardassmulaitis <edvardas.smulaitis@gmail.com>
2026-02-22 00:51:37 +0200

    Create supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,21 @@
+import { createClient } from '@supabase/supabase-js'
+
+export function createAdminClient() {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    {
+      auth: {
+        autoRefreshToken: false,
+        persistSession: false
+      }
+    }
+  )
+}
+
+export function createPublicClient() {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+}
