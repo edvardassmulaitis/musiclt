cd6acc49d8261535ebc8bd6dc1775adb2d65b6cd
edvardassmulaitis <edvardas.smulaitis@gmail.com>
2026-03-02 12:37:56 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import AuthProvider from '@/components/AuthProvider'
+import { AuthProvider } from '@/components/AuthProvider'
 import { SiteShell } from '@/components/SiteShell'
 
 export const metadata: Metadata = {
