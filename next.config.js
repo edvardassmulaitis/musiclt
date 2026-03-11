9a0fd08d3efd60680ed27869f8d706ebd9cf431a
edvardassmulaitis <edvardas.smulaitis@gmail.com>
2026-03-11 17:59:58 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,22 +2,18 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.supabase.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'platform-lookaside.fbsbx.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.fbcdn.net',
-      },
+      { protocol: 'https', hostname: '**.supabase.co' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'platform-lookaside.fbsbx.com' },
+      { protocol: 'https', hostname: '**.fbcdn.net' },
+      // Wikipedia / Wikimedia nuotraukos
+      { protocol: 'https', hostname: 'upload.wikimedia.org' },
+      { protocol: 'https', hostname: '**.wikipedia.org' },
+      // Spotify
+      { protocol: 'https', hostname: 'i.scdn.co' },
+      // YouTube
+      { protocol: 'https', hostname: 'i.ytimg.com' },
+      { protocol: 'https', hostname: 'img.youtube.com' },
     ],
   },
 }
