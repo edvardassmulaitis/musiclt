68e2d787719de3e606f93e7031f1f67ca13c2f8c
edvardassmulaitis <edvardas.smulaitis@gmail.com>
2026-03-16 13:42:59 +0000

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,10 +1,11 @@
 import AdminHeader from '@/components/admin-header'
+import { BackgroundTaskProvider } from '@/components/BackgroundTaskContext'
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <BackgroundTaskProvider>
       <AdminHeader />
       <main>{children}</main>
-    </>
+    </BackgroundTaskProvider>
   )
 }
