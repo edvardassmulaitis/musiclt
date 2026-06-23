fea25917e00eb9c38884d6a1a8a98e1be221e4ac
edvardassmulaitis <edvardas.smulaitis@gmail.com>
2026-06-23 12:07:37 +0300

    Nav: 'Mano profilis' veda i kanonini /@username (ne /vartotojas/username) - venge dublikuoto turinio

--- components/HeaderAuth.tsx
+++ components/HeaderAuth.tsx
@@ -389,7 +389,7 @@ function UserMenu() {
 
           {/* Main links */}
           <div className="py-1.5">
-            {menuItem(username ? `/vartotojas/${username}` : '/auth/profile', (
+            {menuItem(username ? `/@${username}` : '/auth/profile', (
               <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
             ), 'Mano profilis')}
             {menuItem('/mano-muzika', (
